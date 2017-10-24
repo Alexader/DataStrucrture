e1d6e0d7e0b5d3e96a38989be26c766bfa82a8a6
Alexader <tyxj58@gmail.com>
2017-10-24 16:41:26 +0800

    complete a simple version of tree and few functions are supported

--- BinTree.cpp
+++ BinTree.cpp
@@ -1,53 +1,68 @@
 //node in the tree ADT
-template<class E>
-class BinNode {
-private:
+template <class E>
+class BinNode
+{
+  private:
     E ele;
-    BinNode* lc;
-    BinNode* rc;
-public:
-    BinNode(E &element, BinNode* &left, BinNode* &right) {
+    BinNode *lc;
+    BinNode *rc;
+
+  public:
+    BinNode(E &element, BinNode *&left, BinNode *&right)
+    {
         ele = element;
         lc = left;
         right = rc;
     }
-    E& getValue() {
+    E &getValue()
+    {
         return ele;
     }
-    BinNode* & getLetfChild() {
+    BinNode *&getLetfChild()
+    {
         return lc;
     }
-    BinNode* &getRightChild() {
+    BinNode *&getRightChild()
+    {
         return rc;
     }
 }
-//three kind of methods to traveral the tree
-template<class E>
-void preorder(BinNode* subRoot) {
-    if(subRoot==NULL) return;
-    Visit(subRoot);//here in the Visit function, you can do something to operate the node
-    preorder(subRoot->lc);
-    preorder(subRoot->rc);
+
+//binary tree
+template <class E>
+class Tree
+{
+  private:
+    BinNode *root;
+  public:
+    Tree(BinNode *rt) { root = rt; }
+    void traversal() {
+        preorder(root);
+        //choose whatever method you want to complete this(like postorder);
+    }
+    //three kind of methods to traveral the tree
+    void preorder(BinNode *subRoot)
+    {
+        if (subRoot == NULL)
+            return;
+        Visit(subRoot); //here in the Visit function, you can do something to operate the node
+        preorder(subRoot->lc);
+        preorder(subRoot->rc);
+    }
+    inorder(BinNode *subRoot)
+    {
+        if (subRoot == NULL)
+            return;
+        inorder(subRoot->lc);
+        Visit(subRoot);
+        inorder(subRoot->rc);
+    }
+    postorder(BinNode *subRoot)
+    {
+        if (subRoot == NULL)
+            return;
+        postorder(subRoot->lc);
+        postorder(subRoot->rc);
+        Visit(subRoot);
+    }
 }
-template<class E>
-inorder(BinNode* subRoot) {
-    if(subRoot==NULL) return;
-    inorder(subRoot->lc);
-    Visit(subRoot);
-    inorder(subRoot->rc);
-}
-template<class E>
-postorder(BinNode* subRoot) {
-    if(subRoot==NULL) return;
-    postorder(subRoot->lc);
-    postorder(subRoot->rc);
-    Visit(subRoot);
-}
-//binary tree based on array
-template<class E>
-class Tree {
-private:
-    BinNode* root;
-public:
-    
-} 
