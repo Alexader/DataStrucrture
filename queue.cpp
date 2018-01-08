19099b5195c441b0ad0931e1a84803df78bf6b01
Alexader <tyxj58@gmail.com>
2018-01-09 00:29:13 +0800

    add some test code for queue based on array

--- queue.cpp
+++ queue.cpp
@@ -1,3 +1,10 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+void Assert(bool statement, const char* str) {
+    if(statement) cout<<str<<endl;
+}
 //queue ADT
 template<class E>
 class Queue {
@@ -5,13 +12,13 @@ public:
     Queue() {}
     virtual ~Queue() {}
     virtual void clear() = 0;
-    virtual void enqueue(E &ele) = 0;
-    virtual E dequeue() = 0;
+    virtual void enqueue(E ele) = 0;
+    virtual void dequeue(E& temp) = 0;
     virtual int length() = 0;
-}
+};
 //queue based on array
 template<class E>
-class AQueue:public Queue {
+class AQueue:public Queue<E> {
 private:
     E* queue;
     int front;
@@ -20,7 +27,7 @@ private:
 
 public:
     AQueue(int size) {
-        maxSize = size;
+        maxSize = size+1;
         queue = new E[maxSize];
         front = 1;
         rear = 0;
@@ -32,75 +39,99 @@ public:
         front = 1;
         rear = 0;
     }
-    void enqueue(E &ele) {
+    void enqueue(E ele) {
         Assert((rear+2)%maxSize == front, "queue is full");
         rear = (rear+1)%maxSize;
-        queue[rear] = ele;       
+        queue[rear] = ele;   
     }
-    E dequeue() {
+    void dequeue(E& temp) {
         Assert((rear+1)%maxSize == front, "queue is empty");
-        E temp = queue[front];
+        temp = queue[front];
         front = (front+1)%maxSize;
-        return temp;
     }
     int length() {
         return (rear-front+1+maxSize)%maxSize;
     }
-}
+    void print() {
+        if((rear+1)%maxSize == front) { cout<<"queue is empty now"<<endl; return; }
+        cout<<"current element in queue:"<<endl;
+        for (int i = front; i < this->length(); i=(i+1)%maxSize)
+            cout<<queue[i]<<" ";
+        cout<<endl;
+    }
+};
 //queue based on linked list
-template<class E>
-class Node<class E> {
-private:
-    E ele;
-    Node* next;
-public:
-    Node(E& element, Node* nextPtr = NULL) {
-        ele = element;
-        next = nextPtr;
+// template<class E>
+// class Node<class E> {
+// private:
+//     E ele;
+//     Node* next;
+// public:
+//     Node(E& element, Node* nextPtr = NULL) {
+//         ele = element;
+//         next = nextPtr;
+//     }
+// };
+// template<class E>
+// class LQueue:public Queue {
+// private:
+//     Node* top;
+//     Node* bottom;
+//     int size;
+// public:
+//     LQueue() {
+//         size = 0;
+//         head = bottom = NULL;
+//     }
+//     ~LQueue() {
+//         clear();
+//     }
+//     void clear() {
+//         Node* temp = bottom;
+//         while(temp!=NULL) {
+//             bottom = bottom->next;
+//             delete temp;
+//             temp = bottom;
+//         }
+//         top = NULL;
+//     }
+//     void enqueue(E &ele) {
+//         Assert(bottom==NULL, "queue is empty");
+//         bottom = new Node(ele, bottom);
+//         size++;
+//     }
+//     E& dequeue() {
+//         Assert(bottom==NULL, "queue is empty");
+//         Node* temp = bottom;
+//         while(temp->next!=top) {
+//             temp = temp->next;
+//         }
+//         E tempEle;
+//         tempEle = top->ele;
+//         delete top;
+//         top = temp;
+//         size--;
+//         return tempEle;
+//     }
+//     int length() {
+//         return size;
+//     }
+// };
+
+int main(int argc, char const *argv[])
+{
+    AQueue<int> q(10);
+    for(int i=1;i<7;i++) {
+        cout<<i<<" times"<<endl;
+        q.enqueue(i);
     }
-}
-template<class E>
-class LQueue:public Queue {
-private:
-    Node* top;
-    Node* bottom;
-    int size;
-public:
-    LQueue() {
-        size = 0;
-        head = bottom = NULL;
+    cout<<"queue length is :"<<q.length()<<endl;
+    q.print();
+    int temp;
+    for(int i=1;i<7;i++) {
+        q.dequeue(temp);
+        cout<<temp<<" is enqueued"<<endl;
     }
-    ~LQueue() {
-        clear();
-    }
-    void clear() {
-        Node* temp = bottom;
-        while(temp!=NULL) {
-            bottom = bottom->next;
-            delete temp;
-            temp = bottom;
-        }
-        top = NULL;
-    }
-    void enqueue(E &ele) {
-        Assert(bottom==NULL, "queue is empty");
-        bottom = new Node(ele, bottom);
-        size++;
-    }
-    E& dequeue() {
-        Assert(bottom==NULL, "queue is empty");
-        Node* temp = bottom;
-        while(temp->next!=top) {
-            temp = temp->next;
-        }
-        E tempEle;
-        tempEle = top->ele;
-        delete top;
-        top = temp;
-        size--;
-        return tempEle;
-    }
-    int length() {
-        return size;
-    }
+    q.print();
+    return 0;
 }
