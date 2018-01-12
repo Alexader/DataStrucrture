41dc179eb1ef1fed63569d4475f8dd1dbf6a7c23
Alexader <tyxj58@gmail.com>
2018-01-12 21:28:02 +0800

    a simple version of dictionary based on hash table

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,20 +1,26 @@
+#include<iostream>
+#include<assert.h>
+using namespace std;
 //impletion for a hash table based dictionary
 template<typename Key, typename E>
-class KVpair<Key, E> {
+class KVpair {
 private:
     Key key;
     E ele;
 public:
     //Constructor
-    KVpair(Key key, E e) { setPair(key, e); }
+    KVpair() {
+        key = -1; ele = E();
+    }
+    KVpair<Key, E>(Key key, E e) { setPair(key, e); }
     //copy constructor
-    KVpair(const KVpair<Key, E>& pair) {
+    KVpair<Key, E>(const KVpair<Key, E>& pair) {
         key = pair.key;
         ele = pair.ele;
     }
     Key getKey() { return key; }
-    void setPair(Key key, E e) { 
-        key = key;
+    void setPair(Key k, E e) {
+        key = k;
         ele = e;
     }
 };
@@ -27,42 +33,66 @@ private:
     KVpair<Key, E>* HT;
     int count;//count for the length of dictionary
     int current;
-    Key EMPTY_KEY;
+    Key EMPTY_KEY = -1;
     //to get the hash value of key of int type
-    int hash(int key) const {
-        return key % count;
+    int hash(Key key) const {
+        return (int)key % count;
     }
     //to get hash value of key of string
     int hash(char* str) {
-
+        return 0;
     }
     int p(Key key, int pos) {
-        return pos*2;
+        return pos * 2;
     }
 public:
     Dictionary(const int maxSize) {
-        HT = new KVpair<Key, E>[maxSize];
-        count = current = 0;
+        count = maxSize;
+        HT = new KVpair<Key, E>[count];
+        current = 0;
     }
 
     void hashInsert(const Key& K, const E& ele) {
-        int pos = home = hash(K);//home position for each key which may conflict
-        for(int i=0;EMPTY_KEY != HT[pos].getKey();i++) {
-            pos = (home+p(K, i)) % count;
-            Assert(K != HT[pos].getKey(), "Duplicated not allowed");
+        int pos, home, i;
+        pos = home = hash(K);//home position for each key which may conflict
+        for (i = 0; K != HT[pos].getKey() && EMPTY_KEY != HT[pos].getKey() && i<count; i++) {
+            pos = (home + p(K, i)) % count;
         }
-        KVpair<Key, E> temp(K, ele);
-        HT[pos = temp;
+        if (K == HT[pos].getKey())
+            cout << "This key already exist" << endl;
+        else if (i == count)
+            cout << "This hash table is full" << endl;
+        else {
+            cout << "Find a empty place and insert keyword " << K << endl;
+            KVpair<Key, E> temp(K, ele);
+            HT[pos] = temp;
+            current++;
+        }
     }
     bool hashSearch(const Key &K) {
-        int home = pos = hash(K);
+        int pos, home;
+        home = pos = hash(K);
         //you have to be carefull when it goes to infinte loop
-        for(int i=0;EMPTY_KEY != HT[pos].getKey();i++) {
-            if(HT[pos].getKey() == K) return true;
+        for (int i = 0; i<current && EMPTY_KEY != HT[pos].getKey(); i++) {
+            if (HT[pos].getKey() == K) return true;
             pos = (home + p(K, i)) % count;
-            if(i>count) break;
+            if (i>count) break;
         }
-        else return false;
+        return false;
     }
-    int size(){ return current; }
+    int size() { return current; }
+};
+
+int main() {
+    Dictionary<int, int> dic(10);
+    for (int i = 0; i<5; i++) {
+        dic.hashInsert(i, i*i);
+        cout << "done" << endl;
+    }
+    for (int i = 0; i < 10; i+=2) {
+        if (dic.hashSearch(i))
+            cout << "find keyword " << i << endl;
+        else
+            cout << "not found " << i << endl;
+    }
 }
