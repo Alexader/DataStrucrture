76315ad8b1bb27d620a3ff9c5dbcd6d489a1f66d
Alexader <tyxj58@gmail.com>
2018-01-12 14:24:52 +0800

    stack tets finished

--- stack.cpp
+++ stack.cpp
@@ -1,13 +1,15 @@
-const int defaultValue = 20;
+#include<iostream>
+using namespace std;
+int defaultValue = 20;
 template<class Elem>
 class Stack {
 public:
     virtual void clear() = 0;
-    virtual bool push(Elem &ele) = 0;
+    virtual bool push(const Elem ele) = 0;
     virtual bool pop(Elem &ele) = 0;
     virtual bool topValue(Elem &ele) = 0;
-    virtual int length() = 0; 
-}
+    virtual int length() = 0;
+};
 
 template<class Elem>
 class ArrayStack: public Stack<Elem> {
@@ -19,30 +21,51 @@ public:
     ArrayStack(int size = defaultValue) {
         maxSize = size;
         arrayStack = new Elem[maxSize];
-        top = 0;
+        top = -1;
     }
     ~ArrayStack() {
         delete[] arrayStack;
     }
     void clear() {
-        top = 0;
+        top = -1;
     }
-    bool push(Elem &ele) {
-        if(top==0) return false;
-        arrayStack[top++] = ele;
+    bool push(Elem ele) {
+        if(top==maxSize-1) return false;
+        arrayStack[++top] = ele;
         return true;
     }
-    bool pop(Elem &ele) {
-        if(top==0) return false;
-        arrayStack[top--] = ele;
+    bool pop(Elem& ele) {
+        if(top==-1) return false;
+        ele = arrayStack[top--];
         return true;
     }
     bool topValue(Elem &ele) {
-        if(top==0) return false;
+        if(top==-1) return false;
         ele = arrayStack[top];
         return true;
     }
     int length() {
         return top;
     }
+    void print() {
+        for(int i=0;i<this->length();i++)
+            cout<<arrayStack[i]<<" ";
+        cout<<endl;
+    }
+};
+
+int main() {
+    ArrayStack<int> stack(10);
+    for(int i=0;i<10;i++)
+        if(!stack.push(i))
+            cout<<"failure"<<endl;
+    cout<<"this is stack length:"<<stack.length()<<endl;
+    stack.print();
+    int temp;
+    for(int i=0;i<5;i++) {
+        stack.pop(temp);
+        cout<<temp<<" is poping out"<<endl;
+    }
+    stack.topValue(temp);
+    cout<<"top value in stack is:"<<temp<<endl;
 }
